15267fa6d70334282c894b14e74166be9a381214
Isoya Kureno <yerle.nicolas@gmail.com>
2017-10-30 16:24:09 +0100

    recyclerView1

---
+++ app/src/main/java/fr/wcs/hackathon1groupe2/Gift.java
@@ -0,0 +1,23 @@
+package fr.wcs.hackathon1groupe2;
+
+public class Gift {
+    private String title, description;
+    private int thumbnail;
+
+    public Gift() {}
+
+    Gift(String title, String description, int thumbnail) {
+        this.title = title;
+        this.description = description;
+        this.thumbnail = thumbnail;
+    }
+
+    String getTitle() {return title;}
+    public void setTitle(String name) {this.title = name;}
+
+    String getDescription() {return description;}
+    public void setDescription(String description) {this.description = description;}
+
+    int getThumbnail() {return thumbnail;}
+    public void setThumbnail(int thumbnail) {this.thumbnail = thumbnail;}
+}
