7a4dc955721653918a9c5187873404a420b667df
Isoya Kureno <yerle.nicolas@gmail.com>
2017-10-31 00:11:49 +0100

    boutons

--- app/src/main/java/fr/wcs/hackathon1groupe2/TabFragment1.java
+++ app/src/main/java/fr/wcs/hackathon1groupe2/TabFragment1.java
@@ -1,6 +1,8 @@
 package fr.wcs.hackathon1groupe2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +12,14 @@ public class TabFragment1 extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_tab_fragment1, container, false);
+        View view=inflater.inflate(R.layout.activity_tab_fragment1, container, false);
+        FloatingActionButton floatingActionButton=view.findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                Intent toWischActivity=new Intent(getContext(),WischActivity.class);
+                startActivity(toWischActivity);
+            }
+        });
+        return view;
     }
 }
--- app/src/main/java/fr/wcs/hackathon1groupe2/TabFragment3.java
+++ app/src/main/java/fr/wcs/hackathon1groupe2/TabFragment3.java
@@ -1,6 +1,8 @@
 package fr.wcs.hackathon1groupe2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +12,14 @@ public class TabFragment3 extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_tab_fragment3, container, false);
+        View view=inflater.inflate(R.layout.activity_tab_fragment3, container, false);
+        FloatingActionButton floatingActionButton=view.findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                Intent toViewOtherList=new Intent(getContext(),ViewOtherList.class);
+                startActivity(toViewOtherList);
+            }
+        });
+        return view;
     }
 }
