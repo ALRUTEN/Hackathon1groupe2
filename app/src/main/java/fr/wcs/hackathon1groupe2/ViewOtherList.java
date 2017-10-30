4274de1b489583e6f1749d66108816496b47ac99
Isoya Kureno <yerle.nicolas@gmail.com>
2017-10-30 16:51:00 +0100

    réparation login

--- app/src/main/java/fr/wcs/hackathon1groupe2/ViewOtherList.java
+++ app/src/main/java/fr/wcs/hackathon1groupe2/ViewOtherList.java
@@ -16,7 +16,7 @@ public class ViewOtherList extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
 
