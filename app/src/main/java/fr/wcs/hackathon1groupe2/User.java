674fed6c7f810482da4d7961f202b7ba3cfac818
sam64mc <sam64mc@gmail.com>
2017-10-31 00:21:06 +0100

    fix user

--- app/src/main/java/fr/wcs/hackathon1groupe2/User.java
+++ app/src/main/java/fr/wcs/hackathon1groupe2/User.java
@@ -5,28 +5,28 @@ public class User {
     private String user_name;
     private String user_password;
 
-    public User() {}
+    public User() {
+    }
 
-    User(String user_name, String user_password) {
+    public User(String user_name, String user_password) {
         this.user_name = user_name;
         this.user_password = user_password;
     }
 
-    String getUser_name() {
+    public String getUser_name() {
         return user_name;
     }
 
-    void setUser_name(String user_name) {
+    public void setUser_name(String user_name) {
         this.user_name = user_name;
     }
 
-    String getUser_password() {
+    public String getUser_password() {
         return user_password;
     }
 
-    void setUser_password(String user_password) {
+    public void setUser_password(String user_password) {
         this.user_password = user_password;
     }
-
 }
 
