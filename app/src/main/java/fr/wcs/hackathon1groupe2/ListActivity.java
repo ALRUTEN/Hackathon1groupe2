bc1ff23a85729a54bac22615fafa0cf98a92bd15
ALRUTEN <laurentnavarro31@gmail.com>
2017-10-30 19:26:02 +0100

    float

--- app/src/main/java/fr/wcs/hackathon1groupe2/ListActivity.java
+++ app/src/main/java/fr/wcs/hackathon1groupe2/ListActivity.java
@@ -1,13 +1,27 @@
 package fr.wcs.hackathon1groupe2;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ListActivity extends AppCompatActivity {
+
+    FloatingActionButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        FloatingActionButton button = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ListActivity.this, WischActivity.class);
+                startActivity (intent);
+            }
+        });
     }
 }
