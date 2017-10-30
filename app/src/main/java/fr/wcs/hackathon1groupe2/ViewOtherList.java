2c8aef82a5956a84d2b154b2c3fe72f7c58a4e0e
Isoya Kureno <yerle.nicolas@gmail.com>
2017-10-30 14:08:06 +0100

    crea viewOtherList

---
+++ app/src/main/java/fr/wcs/hackathon1groupe2/ViewOtherList.java
@@ -0,0 +1,13 @@
+package fr.wcs.hackathon1groupe2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewOtherList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_other_list);
+    }
+}
